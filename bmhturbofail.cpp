98da791aa4633662b8f9f9b8c3c1f32ed3565e92
agroce <agroce@gmail.com>
2023-05-03 10:43:01 -0700

    fixup

--- bmhturbofail.cpp
+++ bmhturbofail.cpp
@@ -295,8 +295,8 @@ int main() {
   size_t BMH_result = (int) SearchInHorspool(haystack, haystack_length, occ, needle, needle_length);
   size_t Turbo_result = (int) SearchInTurbo(haystack, haystack_length, occ, skip, needle, needle_length);
 
-  printf("BMH: %u", BMH_result);
-  printf("Turbo: %u", Turbo_result);
+  printf("BMH: %ul", BMH_result);
+  printf("Turbo: %ul", Turbo_result);
   
   assert(BMH_result == Turbo_result);
 }
